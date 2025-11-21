93a1462ce1062cebc7d9fc9df100bc8710ad7e11
faris Naser <solonaser9@gmail.com>
2025-11-20 17:41:26 -0800

    added the grid in publish  to make it good

--- src/pages/Publish.jsx
+++ src/pages/Publish.jsx
@@ -3,31 +3,36 @@ import DarkImages from "./images/odinImages.png";
 import Logo from "./desk-lamp-svgrepo-com.svg";
 
 function Publish() {
+  const ContinerStyle = "text-white mx-6 lg:mx-[15%] md:mx-[5%]";
+  const HeadingTitle = "text-4xl  text-white font-[600] py-5 lg:text-6xl";
+  const HeadingText =
+    "w-[86%] text-[#BCBCBC]  text-xl  md:text-2xl md:text-[23px] lg:w-[50%]";
+  const gridSections = "my-4 flex md:block   gap-2 items-start py-4 px-3";
+  const ImagesStyle = "w-10 h-10 gap-2  md:my-2";
+  const LabelStyle = "text-white font-bold  md:block";
+  const ScreenImages = "rounded-xl cursor-pointer ";
+  const ScreenText = "text-center text-[#bcbcbc]  font-normal py-8";
   return (
     <>
-      <div className="text-white mx-6 lg:mx-[15%] md:mx-[5%] ">
+      <div className={ContinerStyle}>
         <div className=" mb-[1rem] mt-[4rem] my-10">
-          <h2 className="text-4xl  text-white font-[600] py-5 lg:text-6xl">
-            Publish instantly.
-          </h2>
-          <p className="w-[86%] text-[#BCBCBC]  text-xl lg:text-2xl lg:w-[50%]">
+          <h2 className={HeadingTitle}>Publish instantly.</h2>
+          <p className={HeadingText}>
             Turn your notes into an online wiki, knowledge base, documentation,
             or digital garden.
-            <p className="text-[#7c3aed] cursor-pointer">
+            <label className="text-[#7c3aed]  block md:inline cursor-pointer">
               <a href="#"> Learn more.</a>
-            </p>
+            </label>
           </p>
           {/*   heading and  text ends here */}
         </div>
 
         <div className="mb-30 lg:mb-10 mt-5 md:flex">
-          <div className="my-4 flex md:block   gap-2 items-start py-4 px-2">
-            <img className="w-10 h-10 gap-2  md:my-2" src={Logo} alt="" />
+          <div className={gridSections}>
+            <img className={ImagesStyle} src={Logo} alt="" />
             <h5 className="text-lg text-[#bcbcbc]">
               {" "}
-              <label className="text-white font-bold  md:block">
-                Seamless editing.{" "}
-              </label>{" "}
+              <label className={LabelStyle}>Seamless editing. </label>{" "}
               <label htmlFor="">
                 {" "}
                 Publish your notes instantly from the Obsidian app, and make it
@@ -36,27 +41,21 @@ function Publish() {
             </h5>
           </div>
           {/*  2nd dev */}
-          <div className="my-4 flex  md:block gap-2 items-start px-3 py-4">
-            <img className="w-10 h-10 gap-2  md:my-2 " src={Logo} alt="" />
+          <div className={gridSections}>
+            <img className={ImagesStyle} src={Logo} alt="" />
             <h5 className="text-lg text-[#bcbcbc]">
               {" "}
-              <label className="text-white font-bold md:block ">
-                {" "}
-                Customization.
-              </label>{" "}
-              Control the look and feel of your site with themes, custom
-              domains, password protection, and more.
+              <label className={LabelStyle}> Customization.</label> Control the
+              look and feel of your site with themes, custom domains, password
+              protection, and more.
             </h5>
           </div>
-          <div className="my-4 flex md:block gap-2 items-start px-3 ">
-            <img className="w-10 h-10 gap-2 md:my-2" src={Logo} alt="" />
+          <div className={gridSections}>
+            <img className={ImagesStyle} src={Logo} alt="" />
             <h5 className="text-lg text-[#bcbcbc]">
               {" "}
-              <label className="text-white font-bold md:block">
-                {" "}
-                Optimized for performance
-              </label>
-              . Obsidian Publish sites are fast, mobile-friendly, and optimized
+              <label className={LabelStyle}> Optimized for performance</label>.
+              Obsidian Publish sites are fast, mobile-friendly, and optimized
               for SEO, no configuration required.
             </h5>
           </div>
@@ -65,7 +64,67 @@ function Publish() {
 
         <div className=" mb-10">
           {" "}
-          <img className="rounded-xl cursor-pointer " src={DarkImages} alt="" />
+          <img className={ScreenImages} src={DarkImages} alt="" />
+          <p className={ScreenText}>
+            {" "}
+            Explore the{" "}
+            <a className="underline hover:text-[#7c3aed]" href="#">
+              Odin Help
+            </a>{" "}
+            site, powered by
+            <a className=" underline pl- hover:text-[#7c3aed]" href="#">
+              Odin Publish.
+            </a>
+          </p>
+        </div>
+        <div>
+          <div className="text-center">
+            <p
+              className="text-4xl md:text-5xl font-[500] text-white pb-7"
+              md:pb-10
+            >
+              It's your time to shine.
+            </p>
+            <button className=" text-white   w-[45%] md:w-[25%] md:text-2xl bg-[#7c3aed]   hover:bg-[7c3aea] rounded-xl text-xl  mb-13 py-5">
+              Get Odin
+            </button>
+            {/*  title section */}
+          </div>
+          <div className="block md:flex items-center gap-4 ">
+            <div className=" bg-[#333] m-2 rounded-lg p-3 my-5">
+              <h2 className="text-xl font-normal text-[#fff] py-5">
+                Join Us On Discord
+              </h2>
+              <p className="text-[#bcbcbc]  text-lg ">
+                Get Help Ask question,meet other Odin user and Learn about their
+                startup{" "}
+              </p>
+            </div>
+            {/* frist grid */}
+
+            <div className="bg-[#333] m-2 rounded-lg p-3  my-5">
+              <h2 className="text-xl font-normal text-[#fff] py-5 ">
+                Discussion Forum
+              </h2>
+              <p className="text-[#bcbcbc]  text-lg">
+                Post feature request Report bags and explore with all
+              </p>
+            </div>
+            {/* 2nd dev  */}
+
+            <div className="bg-[#333] m-2 rounded-lg p-3 my-5">
+              <h2 className="text-xl font-normal text-[#fff] py-5">
+                Developer docs
+              </h2>
+              <p className="text-[#bcbcbc]  text-lg ">
+                Learn how to bulid your own Odin using open API and
+                Documantation{" "}
+              </p>
+            </div>
+            {/* thrid dev */}
+
+            {/* Last Grid section  */}
+          </div>
         </div>
       </div>
     </>
